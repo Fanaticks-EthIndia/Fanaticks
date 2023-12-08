28461fc55e7035b078e8ca1fff9b4612a39ed45b
PawanAK <pawanajjark@gmail.com>
2023-12-08 19:28:15 +0530

    Add react-router-dom dependency

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <li>
+          <NavLink to="/">Dogs</NavLink>
+        </li>
+        <li>
+          <NavLink to="/cats">Cats</NavLink>
+        </li>
+        <li>
+          <NavLink to="/sheeps">Sheeps</NavLink>
+        </li>
+        <li>
+          <NavLink to="/goats">Goats</NavLink>
+        </li>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<div>Dogs Page</div>} />
+        <Route path="/cats" element={<div>Cats Page</div>} />
+        <Route path="/sheeps" element={<div>Sheeps Page</div>} />
+        <Route path="/goats" element={<div>Goats Page</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
