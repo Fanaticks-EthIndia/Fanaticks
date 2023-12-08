ea9af4f62504b6279b447d361a01069d718fd757
PawanAK <pawanajjark@gmail.com>
2023-12-09 01:56:46 +0530

    Refactor Home page layout and add images

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,29 @@
 const Home = () => {
   return (
     <div>
-      <div className="hero-section">
-        <div className="hero-section content">
-          <div className="hero-section-title">
-            Elevate Events Own Experiences
+      <div className="hero-section flex items-center justify-center h-screen relative">
+        {/* Top Right Image */}
+        <img
+          src="top-right-image.jpg" // Replace with the actual path to your image
+          alt="Top Right Image"
+          className="absolute top-0 right-0 w-24 h-24 mr-8 mt-8"
+        />
+
+        {/* Bottom Left Image */}
+        <img
+          src="bottom-left-image.jpg" // Replace with the actual path to your image
+          alt="Bottom Left Image"
+          className="absolute bottom-0 left-0 w-24 h-24 ml-8 mb-8"
+        />
+
+        <div className="hero-section content text-center">
+          <div className="hero-section-title text-4xl font-bold mb-4">
+            Elevate Events
           </div>
-          <div className="hero-section-subtitle">
+          <div className="hero-section-title text-4xl font-bold mb-4">
+            Own Experiences
+          </div>
+          <div className="hero-section-subtitle text-lg mb-8">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
             diam justo, varius non sapien id.
           </div>
@@ -15,7 +32,7 @@ const Home = () => {
               Create Event
             </button>
             <button className="hero-button discover bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Discover
+              Discover Events
             </button>
           </div>
         </div>
