1867db0ebb9594c3c34d66f78683dfe0f961cdea
PawanAK <pawanajjark@gmail.com>
2023-12-08 20:37:17 +0530

    Add ConnectWallet functionality and update Home page

--- client/src/components/ConnectWallet.jsx
+++ client/src/components/ConnectWallet.jsx
@@ -1,11 +1,35 @@
-
+import  { useEffect, useState } from 'react';
 
 const ConnectWallet = () => {
+  const [account, setAccount] = useState('');
+
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(accounts[0]);
+      } catch (error) {
+        console.error("Failed to connect to Metamask");
+      }
+    } else {
+      console.log("Please install Metamask");
+    }
+  };
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on('accountsChanged', (accounts) => {
+        setAccount(accounts[0]);
+      });
+    }
+  }, []);
+
   return (
     <div>
-      <button>Connect ConnectWallet</button>
+      <button onClick={connectWallet}>Connect Wallet</button>
+      {account && <p>Connected: {account}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default ConnectWallet
+export default ConnectWallet;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,14 +3,7 @@
 const Home = () => {
   return (
     <div>
-      <section>
-        <h2>Welcome to the Landing Page</h2>
-        <p>This is the home page of your application.</p>
-      </section>
-      <section>
-        <h3>Featured Events</h3>
-        {/* Add featured events or content related to your landing page */}
-      </section>
+      <h1>Homeeeeeeeeee</h1>
     </div>
   );
 };
