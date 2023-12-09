e3d78ab1401a48a734dcbdd49338e8c65b76a148
Shivang Parmar <shivangparmar537@gmail.com>
2023-12-09 14:09:25 +0530

    made changes to Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,31 +1,34 @@
 import { Link } from "react-router-dom";
 import ConnectWallet from "./ConnectWallet";
+import logo from "../assets/images/logo.svg";
 
 const NavBar = () => {
   return (
-    <div className="Navbar w-fullfixed top-0 z-50">
+    <div className="Navbar w-full fixed top-2 z-50">
       <div className="w-[1175px] mx-auto h-[46px] flex justify-between items-center">
-        <div className="FanaticksLogo h-[34px] flex items-center">
+        <div className="FanaticksLogo h-[34px] flex justify-center items-center gap-2">
+          <img src={logo} className="h-8 w-fit" />
           <Link
-            to= "/"
-            className="Fanaticks black text-zinc-400 text-[32px] font-normal font-['Quanta Grotesk Pro']" >
+            to="/"
+            className="Fanaticks black  text-[32px] font-normal font-secondary text-primary" >
+
             Fanaticks
           </Link>
         </div>
         <div className="Frame3 flex items-center space-x-8 font-primary">
           <Link
-            to="/about"
-            className="text-black text-lg font-normal leading-[27px]">
+            to="/#about"
+            className="text-white text-lg font-normal leading-[27px]">
             About
           </Link>
           <Link
             to="/discover"
-            className="text-black text-lg font-normal leading-[27px]">
+            className="text-white text-lg font-normal leading-[27px]">
             Discover
           </Link>
           <Link
             to="/create-event"
-            className="text-black text-lg font-normal leading-[27px]">
+            className="text-white text-lg font-normal leading-[27px]">
             Create Event
           </Link>
         </div>
