737c5b6cd9c601693e1421a16339ecc2726f7844
Mihir Kuma Singh <mihir39nr@gmail.com>
2023-12-10 04:36:45 +0530

    Push Started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,27 +5,24 @@ import Home from "./pages/Home";
 import Discover from "./pages/Discover";
 import CreateEvent from "./pages/CreateEvent";
 import Footer from "./components/Footer";
-import { AnonAadhaarProvider } from "anon-aadhaar-react";
-
-const app_id = '820447064610190777556340711364689044793307693056';
+import Everydayscreen from "./components/push";
 
 const App = () => {
   return (
-    <AnonAadhaarProvider _appId={app_id}>
-      <div className="flex flex-col ">
-        <Router>
-          <NavBar />
-          <div className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/discover" element={<Discover />} />
-              <Route path="/create-event" element={<CreateEvent />} />
-            </Routes>
-          </div>
-        </Router>
-        <Footer />
-      </div>
-    </AnonAadhaarProvider>
+    <div className="flex flex-col ">
+      <Router>
+        <NavBar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/discover" element={<Discover />} />
+            <Route path="/create-event" element={<CreateEvent />} />
+            <Route path="/push" element={<Everydayscreen/>} />
+          </Routes>
+        </div>
+      </Router>
+      <Footer />
+    </div>
   );
 };
 
