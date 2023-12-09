8349c09f3919d46594f11b4714ec5162d52d6af2
PawanAK <pawanajjark@gmail.com>
2023-12-09 07:28:37 +0530

    Add copy to clipboard functionality in ConnectWallet component

--- client/src/components/ConnectWallet.jsx
+++ client/src/components/ConnectWallet.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const ConnectWallet = () => {
   const [account, setAccount] = useState("");
+  const [copied, setCopied] = useState(false);
 
   const connectWallet = async () => {
     if (window.ethereum) {
@@ -26,9 +27,13 @@ const ConnectWallet = () => {
     }
   }, []);
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(account);
+  const copyToClipboard = async () => {
+    await navigator.clipboard.writeText(account);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 200); // Reset after 2 seconds
   };
+
+  const truncatedAddress = account.substring(0, 9) + "...";
 
   return (
     <div>
@@ -39,11 +44,21 @@ const ConnectWallet = () => {
           Connect Wallet
         </button>
       ) : (
-        <button
-          className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={copyToClipboard}>
-          <p>{account}</p>
-        </button>
+        <div className="relative inline-block">
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full relative"
+            onClick={copyToClipboard}>
+            <p>{truncatedAddress}</p>
+            {copied && (
+              <div className="absolute top-0 right-0 mt-2">
+                <div className="bg-gray-800 text-white py-1 px-2 rounded opacity-75">
+                  Copied!
+                  <span className="text-gray-800">&#9654;</span>
+                </div>
+              </div>
+            )}
+          </button>
+        </div>
       )}
     </div>
   );
