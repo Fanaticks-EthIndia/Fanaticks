acc21496408dd06eecbb22903710d70136801d8c
Shivang Parmar <shivangparmar537@gmail.com>
2023-12-09 14:16:44 +0530

    made some changes to the ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/Navbar";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Discover from "./pages/Discover";
 import CreateEvent from "./pages/CreateEvent";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen mx-10">
       <Router>
         <NavBar />
         <div className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
             <Route path="/discover" element={<Discover />} />
             <Route path="/create-event" element={<CreateEvent />} />
           </Routes>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,10 +7,18 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        primary: ['Space Grotesk','sans-serif'],
-        secondary: ['Quanta Grotesk Pro','sans-serif']
+        primary: ['Space Grotesk', 'sans-serif'],
+        secondary: ['Quanta Grotesk Pro', 'sans-serif']
+      },  
+      colors: {
+        background: '#18181A',
+        primary: '#542D72',
+        secondary: '#A566A6',
+        drakGray: '#333333',
+        lightGray: '#71706C'
       }
     },
+
   },
   plugins: [],
 }
