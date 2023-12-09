4f0efb470235fee780b558006ae6bf1886af7a09
PawanAK <pawanajjark@gmail.com>
2023-12-10 04:23:54 +0530

    Add AnonAadhaar integration to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,22 +5,27 @@ import Home from "./pages/Home";
 import Discover from "./pages/Discover";
 import CreateEvent from "./pages/CreateEvent";
 import Footer from "./components/Footer";
+import { AnonAadhaarProvider } from "anon-aadhaar-react";
+
+const app_id = '820447064610190777556340711364689044793307693056';
 
 const App = () => {
   return (
-    <div className="flex flex-col ">
-      <Router>
-        <NavBar />
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/discover" element={<Discover />} />
-            <Route path="/create-event" element={<CreateEvent />} />
-          </Routes>
-        </div>
-      </Router>
-      <Footer />
-    </div>
+    <AnonAadhaarProvider _appId={app_id}>
+      <div className="flex flex-col ">
+        <Router>
+          <NavBar />
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/discover" element={<Discover />} />
+              <Route path="/create-event" element={<CreateEvent />} />
+            </Routes>
+          </div>
+        </Router>
+        <Footer />
+      </div>
+    </AnonAadhaarProvider>
   );
 };
 
